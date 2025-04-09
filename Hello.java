0b64a75a9d3da355f6c640fc3eefed32773b3b20
anand5206 <ananddeepakjadhav@gmail.com>
2025-04-09 12:04:47 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
-public class Hello
-{
-  public static void main(String[] args)
-  {
-    System.out.println("hello world");
-  }
-} 
+// Your First Program
+
+class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
+}
